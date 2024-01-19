3357862e9e5ae045339dc20f69db945593e182dd
Nirlesh <Null>
2024-01-19 22:49:32 +0530

    Making Calculator

---
+++ main.py
@@ -0,0 +1,7 @@
+a=int(input("Enter any number Which you want to Double (Square):"))
+number=int(input("Enter Number of multiplication:"))
+
+print(a)
+for number in range (number - 1):
+    print(a*a)
+    a=a*a
